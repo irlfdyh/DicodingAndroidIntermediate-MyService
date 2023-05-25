0851afb92d1b7b49bb417b0ebd2386b2543bf3f8
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-25 14:03:04 +0700

    add simple counter at background service

--- app/src/main/java/com/dicoding/intermediate/service/myservice/MyBackgroundService.kt
+++ app/src/main/java/com/dicoding/intermediate/service/myservice/MyBackgroundService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyBackgroundService : Service() {
 
@@ -11,13 +12,30 @@ class MyBackgroundService : Service() {
         internal val TAG = MyBackgroundService::class.java.simpleName
     }
 
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("Not yet implemented")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "Service dijalankan...")
+        serviceScope.launch {
+            for (i in 1..10) {
+                delay(1000)
+                Log.d(TAG, "Do something $i")
+            }
+            stopSelf()
+            Log.d(TAG, "Service dihentikan")
+        }
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "onDestroy: Service dihentikan")
     }
 
 }
