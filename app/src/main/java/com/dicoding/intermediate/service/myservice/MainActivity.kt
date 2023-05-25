0f7cd7ab075869872bcb360185268aa49b1dbd1e
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-25 14:32:19 +0700

    implement foreground service

--- app/src/main/java/com/dicoding/intermediate/service/myservice/MainActivity.kt
+++ app/src/main/java/com/dicoding/intermediate/service/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.intermediate.service.myservice
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.intermediate.service.myservice.databinding.ActivityMainBinding
@@ -13,15 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupService()
+        setupBackgroundService()
+        setupForegroundService()
     }
 
-    private fun setupService() {
+    private fun setupBackgroundService() {
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener {
             startService(serviceIntent)
         }
         binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+    }
+
+    private fun setupForegroundService() {
+        val serviceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(serviceIntent)
+            } else {
+                startService(serviceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
             stopService(serviceIntent)
         }
     }
