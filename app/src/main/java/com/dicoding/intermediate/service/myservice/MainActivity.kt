071b21c1e8451fdf9350b8465943eabe632c1710
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-25 15:40:59 +0700

    implement bound service

--- app/src/main/java/com/dicoding/intermediate/service/myservice/MainActivity.kt
+++ app/src/main/java/com/dicoding/intermediate/service/myservice/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.dicoding.intermediate.service.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
         setupBackgroundService()
         setupForegroundService()
+        setupBoundService()
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -76,6 +80,48 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopForegroundService.setOnClickListener {
             stopService(serviceIntent)
+        }
+    }
+
+    /**
+     * Bound service settings
+     */
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundStatus = false
+        }
+    }
+
+    private fun getNumberFromService() {
+        boundService.numberLiveData.observe(this) { number ->
+            binding.tvBoundServiceNumber.text = number.toString()
+        }
+    }
+
+    private fun setupBoundService() {
+        val boundServiceIntent = Intent(this, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (boundStatus) {
+            unbindService(connection)
+            boundStatus = false
         }
     }
 
